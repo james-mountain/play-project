3f3db9d0e22cbb80910ae46991c85897d377bc1d
James Mountain <james.w.mountain@gmail.com>
2017-07-24 14:50:15 +0000

    Added some task assigned routes
    Added some actions to the application as part of a test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,8 +9,23 @@ class Application extends Controller {
     Ok("Hello World")
   }
 
+  def staticprint : Action[AnyContent] = Action {
+    Ok("Hello whoever that may be")
+  }
+
   def print(name : String) : Action[AnyContent] = Action {
-    Ok("Hello" + name)
+    Ok("Hello " + name)
+  }
+
+  def optionalPrint(option : Option[String]) : Action[AnyContent] = Action {
+    option match {
+      case Some(str) => Ok("Option found: " + str)
+      case None => Ok("No option found!")
+    }
+  }
+
+  def redirectionToGenericName: Action[AnyContent] = Action {
+    Redirect(routes.Application.print("genericname"))
   }
 
 }

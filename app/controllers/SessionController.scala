a04e475e8c8ba72421f79c638c1c2ffea2b68ec4
James Mountain <james.w.mountain@gmail.com>
2017-07-25 13:11:42 +0000

    Refactored code and added dynamic sesson data
    Inspection of session data dynamically

--- app/controllers/SessionController.scala
+++ app/controllers/SessionController.scala
@@ -1,21 +1,22 @@
 package controllers
 
+import play.api.libs.json.Json
 import play.api.mvc._
 
 class SessionController extends Controller {
   def addToSessionAction: Action[AnyContent] = Action {
-    Ok("Added to session").withSession("sessiondata" -> "somedataforsession")
+    Ok(Json.obj("message" -> "success")).withSession("sessiondata" -> "somedataforsession")
   }
 
   def printSessionContentsAction: Action[AnyContent] = Action { implicit request =>
     request.session.get("sessiondata").map {data =>
-      Ok("Session data found: " + data)
+      Ok(Json.obj("message" -> "success", "sessiondata" -> data))
     }.getOrElse {
-      Redirect(routes.Application.badRequest(Some("No session data found.")))
+      Ok(Json.obj("message" -> "failure"))
     }
   }
 
   def removeFromSessionAction: Action[AnyContent] = Action { implicit request =>
-    Ok("Removed data from session").withSession(request.session - "sessiondata")
+    Ok(Json.obj("message" -> "success")).withSession(request.session - "sessiondata")
   }
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,30 @@
 var nilfunc = function() {};
+var animtime = 200;
 
-function cookieDisplayGet(result) {
+function successMessage(result, message) {
     if (result.message === "success") {
-        $("#hidewrap").show(400, nilfunc);
-        $("#hiddenmessage").html("Successfully got new cookie.");
+        $("#hidewrap").show(animtime, nilfunc);
+        $("#hiddenmessage").html(message);
+    }
+}
+
+function cookieRedisplay(result) {
+    if (result.message === "success") {
+        $("#hidewrap").show(animtime, nilfunc);
+        $("#hiddenmessage").html("Cookie name: " + result.cookiename + " | Cookie value: " + result.cookievalue);
+    } else {
+        $("#hidewrap").show(animtime, nilfunc);
+        $("#hiddenmessage").html("No cookie data found!");
+    }
+}
+
+function sessionRedisplay(result) {
+    if (result.message === "success") {
+        $("#hidewrap").show(animtime, nilfunc);
+        $("#hiddenmessage").html("Session data: " + result.sessiondata);
+    } else {
+        $("#hidewrap").show(animtime, nilfunc);
+        $("#hiddenmessage").html("No session data found!");
     }
 }
 
@@ -12,21 +33,11 @@ function getCookieRequest() {
         url: "/getnewcookie",
         type: 'GET',
         success: function(result) {
-            $("#hidewrap").hide(400, function() {
-                cookieDisplayGet(result)
+            $("#hidewrap").hide(animtime, function() {
+                successMessage(result, "Successfully got new cookie.");
             })
         }
     });
-}
-
-function cookieRedisplay(result) {
-    if (result.message === "success") {
-        $("#hidewrap").show(400, nilfunc);
-        $("#hiddenmessage").html("Cookie name: " + result.cookiename + " | Cookie value: " + result.cookievalue);
-    } else {
-        $("#hidewrap").show(400, nilfunc);
-        $("#hiddenmessage").html("No cookie data found!");
-    }
 }
 
 function showCookieRequest() {
@@ -34,18 +45,11 @@ function showCookieRequest() {
         url: "/showcookie",
         type: 'GET',
         success: function(result) {
-            $("#hidewrap").hide(400, function() {
-                cookieRedisplay(result)
+            $("#hidewrap").hide(animtime, function() {
+                cookieRedisplay(result);
             })
         }
     });
-}
-
-function cookieRevokeGet(result) {
-    if (result.message === "success") {
-        $("#hidewrap").show(400, nilfunc);
-        $("#hiddenmessage").html("Revoked cookie.");
-    }
 }
 
 function removeCookieRequest() {
@@ -53,8 +57,44 @@ function removeCookieRequest() {
         url: "/revokecookie",
         type: 'GET',
         success: function(result) {
-            $("#hidewrap").hide(400, function() {
-                cookieRevokeGet(result)
+            $("#hidewrap").hide(animtime, function() {
+                successMessage(result, "Revoked cookie.");
+            })
+        }
+    });
+}
+
+function getSessionRequest() {
+    $.ajax({
+        url: "/addtosession",
+        type: 'GET',
+        success: function(result) {
+            $("#hidewrap").hide(animtime, function() {
+                successMessage(result, "Successfully added data to session.");
+            })
+        }
+    });
+}
+
+function showSessionDataRequest() {
+    $.ajax({
+        url: "/showsessiondata",
+        type: 'GET',
+        success: function(result) {
+            $("#hidewrap").hide(animtime, function() {
+                sessionRedisplay(result);
+            })
+        }
+    });
+}
+
+function removeSessionDataRequest() {
+    $.ajax({
+        url: "/removefromsession",
+        type: 'GET',
+        success: function(result) {
+            $("#hidewrap").hide(animtime, function() {
+                successMessage(result, "Removed data from session.");
             })
         }
     });
